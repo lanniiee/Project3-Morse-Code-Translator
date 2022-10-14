4a6ceb38981d41e4ad6bd691c0ee4df0d08f1777
lanniiee <lav250397@hotmail.com>
2022-10-14 15:50:59 +0100

    add in reset function

--- files/main.js
+++ files/main.js
@@ -3,12 +3,18 @@ import { translateToMorseCode } from "./morse-code-translate.js";
 
 const inputBox = document.querySelector("#input");
 const display = document.querySelector("#display");
-const translateButton = document.querySelector("#btn");
+const translateButton = document.querySelector("#translate");
+const resetBtn = document.querySelector("#reset");
 
 const translate = () => {
     const input = inputBox.value;
     display.innerHTML = translateToMorseCode(input);
 }
 
+const reset = () => {
+    display.textContent = "";
+    inputBox.value = "";
+}
 
-translateButton.addEventListener("click", translate)
+translateButton.addEventListener("click", translate);
+resetBtn.addEventListener("click", reset);
